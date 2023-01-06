99a92d94009b776a50a6e6b008e8ff6018332192
Fagner Joaquim <fagneralves.xone@gmail.com>
2023-01-05 22:56:55 -0300

    #01: detalhes na descrição
    - HTML e CSS prontos faltando implementar somente o JAVASCRIPT

---
+++ troca-cores/app.js
@@ -0,0 +1,9 @@
+function trocaCor () {
+    alert('The button')
+
+    let cor   = document.getElementById('cor');
+    let corpo = document.getElementById('corpo');
+
+    corpo.style.backgroundColor = "#fff";
+}
+
