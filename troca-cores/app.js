83cdb11b9f23d59c2d43ad64dc49ba3e529f2bcc
Fagner Joaquim <fagneralves.xone@gmail.com>
2023-01-09 18:46:23 -0300

    detalhes na descrição
    Implementando função que gerar um número hexadecimal

--- troca-cores/app.js
+++ troca-cores/app.js
@@ -1,9 +1,37 @@
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  function setRandomColor() {
+    $("#colorpad").css("background-color", getRandomColor());
+  }
+
+
+function gerarNumero (numero) {
+    numero = console.log(Math.floor(Math.random() * 10));
+}
+
+function gerarTexto (texto) {
+    console.log(texto);
+}
+
 function trocaCor () {
-    alert('The button')
 
-    let cor   = document.getElementById('cor');
-    let corpo = document.getElementById('corpo');
+    console.log(getRandomColor());
 
-    corpo.style.backgroundColor = "#fff";
+    // let corpo = document.getElementsByClassName('corpoPrincipal'); 
+
+    let corpo1 = document.getElementById('corpoPrincipal');
+
+    corpo1.style.backgroundColor = getRandomColor();
+
+    // console.log(corpo);
+    console.log(corpo1);
 }
 
